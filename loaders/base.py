ae88eaa96bdbd2b6f43b70a8be9bedbce418d9b8
duchesneaumathieu <duchesneau.mathieu@gmail.com>
2020-05-27 23:19:39 -0400

    save work

---
+++ loaders/base.py
@@ -0,0 +1,96 @@
+import os
+import numpy as np
+from radbm.utils.os import StateObj
+
+def locate_dataset_dirs(dataset, path):
+    # <path> if path is not None
+    # $DATASETS_DIR/<dataset>
+    # ./datasets/<dataset>
+    # ./<dataset>
+    paths = list()
+    if path is not None:
+        if os.path.isfile(path):
+            paths.append(os.path.dirname(path))
+        else: paths.append(path)
+    if 'DATASETS_DIR' in os.environ:
+        tmp = set()
+        env_path = [p for p in os.environ['DATASETS_DIR'].split(':') if p not in tmp and tmp.add(p) is None and p]
+        paths.extend(env_path)
+    paths.extend(['./datasets', '.'])
+    
+    output = list()
+    for p in paths:
+        while p.endswith('/'):
+            p = p[:-1] #remove ending /
+        if p.endswith(dataset):
+            if os.path.isdir(p):
+                output.append(p)
+        elif os.path.isdir(os.path.join(p, dataset)):
+            output.append(os.path.join(p, dataset))
+    return output
+    
+def locate_dataset_files(dataset, path, file):   
+    # <path> if not None (path endswith file)
+    # dir/<file> for dir in locate_dataset_dirs
+    # <file>
+    paths = list()
+    if path is not None:
+        if path.endswith(file) and os.path.isfile(path):
+            paths.append(path)
+        else:
+            join = os.path.join(path,file)
+            if os.path.isfile(join): paths.append(join)
+    for d in locate_dataset_dirs(dataset, path):
+        f = os.path.join(d,file)
+        if os.path.isfile(f): paths.append(f)
+    if os.path.isfile(file): paths.append(file)
+        
+    tmp = set()
+    return [p for p in paths if p not in tmp and tmp.add(p) is None and p]
+    return paths
+
+class Loader(StateObj):
+    def __init__(self, name, mode, rng=np.random):
+        modes = self.get_available_modes()
+        if mode not in modes:
+            msg = 'mode must be in {}, got {}'
+            raise ValueError(msg.format(modes, mode))
+        self.name=name
+        self.mode=mode
+        self.rng=rng
+        
+    def get_rng_state_hash(self):
+        s = self.rng.get_state()
+        #don't hash string (e.g. s[0]) since string's hash are salted at each session
+        return hash(tuple(s[1])+s[2:])
+    
+    def __repr__(self):
+        s = self.rng.get_state()
+        r = 'Loader: {}\nMode: {}\nRNG\'s State (hash): {}'
+        return r.format(
+            self.__class__.__name__,
+            self.mode,
+            self.get_rng_state_hash()
+        )
+    
+    def get_available_modes(self):
+        return {
+            'Uunsupervised',
+            'Class',
+            'Relational',
+            'Relational_list',
+            'Relational_matrix',
+        }
+    
+    def get_state(self):
+        return self.rng.get_state()
+    
+    def set_state(self, state):
+        #fork rng before updating since it might be global
+        self.rng = np.random.RandomState()
+        self.rng.set_state(state)
+        return self
+    
+    #for easy sharing rng across multiple object
+    def get_rng(self): return self.rng
+    def set_rng(self, rng): self.rng=rng; return rng
---
+++ loaders/mnist.py
@@ -0,0 +1,72 @@
+import numpy as np
+import gzip, pickle
+from radbm.loaders.base import Loader, locate_dataset_dirs, locate_dataset_files
+
+class Mnist(Loader):
+    def __init__(self, name, mode, rng=np.random, path=None):
+        super().__init__(name, mode, rng)
+        file_paths = locate_dataset_files('Mnist', path, 'mnist.pkl.gz')
+        if len(file_paths) == 0: raise FileNotFoundError('could not locate mnist.pkl.gz')
+        with gzip.open(file_paths[0], 'rb') as f:
+            self.train_xy, self.valid_xy, self.test_xy = pickle.load(f, encoding='latin1')
+            self.train_x, self.train_y = self.train_xy
+            self.valid_x, self.valid_y = self.valid_xy
+            self.test_x, self.test_y = self.test_xy
+            
+        self.train()
+        
+    def train(self): self.data_x, self.data_y = self.train_xy; return self
+    def valid(self): self.data_x, self.data_y = self.valid_xy; return self
+    def test(self): self.data_x, self.data_y = self.test_xy; return self
+        
+    def batch(self, size=None, index=None, replace=True):
+        if index is not None or not replace:
+            raise NotImplementedError('not implemented yet')
+        ids = self.rng.randint(0, len(self.data_y), size)
+        x = self.data_x[ids]
+        y = self.data_y[ids]
+        return x, y
+    
+class MnistClass(Mnist):
+    def __init__(self, sigma, name, mode, rng=np.random):
+        if mode is not 'Class':
+            raise NotImplementedError('Only Class mode is implemented for NoisyMnist')
+        super().__init__(name, mode, rng)
+        self.sigma=sigma
+        
+    def batch(self, size=None, index=None, replace=True):
+        x, y = super().batch(size, index, replace)
+        q = x + self.rng.normal(0, self.sigma, x.shape)
+        d = x + self.rng.normal(0, self.sigma, x.shape)
+        return q, d, np.eye(size, dtype=np.uint8)
+    
+class NoisyMnist(Mnist):
+    def __init__(self, sigma, name, mode, rng=np.random):
+        if mode is not 'Relational':
+            raise NotImplementedError('Only Relational mode is implemented for NoisyMnist')
+        super().__init__(name, mode, rng)
+        self.sigma=sigma
+        
+    def get_relation_prob(self):
+        return 1/len(self.data_x)
+    
+    def get_relation_log_prob(self):
+        return -np.log(len(self.data_x))
+        
+    def build_any(self, size=None, index=None, replace=True):
+        if size is not None or replace:
+            raise NotImplementedError('not implemented yet')
+        x = self.data_x if index is None else self.data_x[index]
+        return x + self.rng.normal(0, self.sigma, x.shape)
+    
+    def build_queries(self, size=None, index=None, replace=True):
+        return self.build_any(size, index, replace) #because the tasks is symmetric
+    
+    def build_documents(self, size=None, index=None, replace=True):
+        return self.build_any(size, index, replace) #because the tasks is symmetric
+    
+    def batch(self, size=None, index=None, replace=True):
+        x, y = super().batch(size, index, replace)
+        q = x + self.rng.normal(0, self.sigma, x.shape)
+        d = x + self.rng.normal(0, self.sigma, x.shape)
+        return q, d, np.eye(size, dtype=np.uint8)
