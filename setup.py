b752e1db436ec2d9bb0f0415f715043f315f2d64
duchesneaumathieu <duchesneau.mathieu@gmail.com>
2020-11-02 14:29:20 -0500

    hopefully packaging fileid.json

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/duchesneaumathieu/radbm",
     packages=setuptools.find_packages(),
-    data_files=[('radbm/utils/gdrive', ['radbm/utils/gdrive/fileid.json'])],
+    package_data={'radbm.utils.gdrive': ['fileid.json']},
     classifiers=[
         "Programming Language :: Python :: 3",
         'Intended Audience :: Science/Research',
